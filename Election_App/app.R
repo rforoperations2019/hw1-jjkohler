cd9a01c15a9338ad37054931ab6839e244911bbb
jjkohler <42879120+jjkohler@users.noreply.github.com>
2019-09-07 11:05:01 -0400

    More inputs

--- Election_App/app.R
+++ Election_App/app.R
@@ -4,6 +4,7 @@ library(DT)
 library(stringr)
 library(dplyr)
 library(tools)
+library(plotly)
 library(shinyWidgets)
 election_data <- read.csv("2016_1984.csv", check.names=FALSE)
 # Define UI for application that draws a histogram
@@ -23,7 +24,17 @@ ui <- fluidPage(
                         value = 1984,
                         step = 4,
                         sep = '',
-                        ticks = TRUE)
+                        ticks = TRUE),
+            
+            # checkboxGroupButtons(
+            #     inputId = "somevalue", label = "Data Aggregation Level :", 
+            #     choices = c("Choice A", "Choice B"), 
+            #     justified = TRUE, status = "primary",
+            #     checkIcon = list(yes = icon("ok", lib = "glyphicon"), no = icon("remove", lib = "glyphicon")),
+                
+            radioButtons(inputId="choice", label="Aggregation Level :",
+                             choices=c("State","County"))
+            # )
         ),
 
         # Show a plot of the generated distribution
@@ -31,6 +42,8 @@ ui <- fluidPage(
            plotOutput("distPlot"),
            
            plotOutput("OtherVoting"),
+           
+           plotOutput("votePie"),
            
            # Show data table ---------------------------------------------
            DT::dataTableOutput(outputId = "polarization")
@@ -66,6 +79,9 @@ server <- function(input, output) {
         test
         })
 
+    agg <- reactive({aggregate(x = data()[c('Total Votes', 'Dem Votes', 'Rep Votes', 'Other Votes')],
+                           by = list(unique.values = data()$State),
+                           FUN = sum)})
 
 
     output$distPlot <- renderPlot({
@@ -73,7 +89,8 @@ server <- function(input, output) {
         # draw a histogram of partisan divide paterns
         h <- hist(winner()[,2], breaks = 75, border = 'white')
         cuts <- cut(h$breaks, c(-50,-2,2,50))
-        plot(h, ylim = c(0,250), xlim = c(-60,60), col=c("dodgerblue2","mediumslateblue","brown2")[cuts])
+        plot(h, ylim = c(0,250), xlim = c(-60,60), col=c("dodgerblue2","mediumslateblue","brown2")[cuts],
+             main='Number of Counties by Partisan Lean Percentage')
     })
     
     output$OtherVoting <- renderPlot({
@@ -84,6 +101,20 @@ server <- function(input, output) {
         plot(h, xlim = c(0,.4), ylim = c(0,300), col=c("palegreen2","palegreen3","palegreen4")[cuts])
     })
     
+    output$VotePie <- renderPlot({
+        p <- plot_ly(data, labels = ~Categorie, values = ~X1960, type = 'pie') %>%
+            layout(title = 'United States Personal Expenditures by Categories in 1960',
+                   xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                   yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+        
+            #     pie <- bp + coord_polar("y", start=0)
+    # pie + scale_fill_brewer("Blues") + blank_theme +
+    #     theme(axis.text.x=element_blank())+
+    #     geom_text(aes(y = value/3 + c(0, cumsum(value)[-length(value)]), 
+    #                   label = percent(value/100)), size=5)
+    })
+        
+        
     output$polarization <- DT::renderDataTable(
     DT::datatable(data = data()[,1:7], 
                   options = list(pageLength = 10), 
